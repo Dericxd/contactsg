9f888c1e30b410c8289307923a75337a881cb310
eric <edi@sinapsys.global>
2026-07-07 07:39:27 -0400

    first config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,31 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig, Injectable, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideHttpClient, withFetch } from '@angular/common/http';
+import { HttpClient, provideHttpClient, withFetch } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+Injectable({
+  providedIn: 'root'
+});
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
-    provideHttpClient(withFetch()),
+    // provideHttpClient(withFetch()),
+    provideHttpClient(),
   ]
 };
+
+//? conexion con la base de datos 
+export class DataService {
+
+  private apiUrl = 'http://localhost:3000/api/datos';
+
+  constructor(private http: HttpClient) {}
+
+  getData():Observable<any> {
+    return this.http.get<any>(this.apiUrl)
+  }
+}
